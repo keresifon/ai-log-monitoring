62bb3a3a07a99052c03ee2beb282c5838fb5157e
Kere Ekpenyong <Kere.Ekpenyong@ibm.com>
2026-01-13 20:32:40 -0500

    Update LogIngestionServiceApplicationTests.java

--- backend/log-ingestion/src/test/java/com/ibm/aimonitoring/ingestion/LogIngestionServiceApplicationTests.java
+++ backend/log-ingestion/src/test/java/com/ibm/aimonitoring/ingestion/LogIngestionServiceApplicationTests.java
@@ -2,8 +2,17 @@ package com.ibm.aimonitoring.ingestion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:postgresql://localhost:5432/aimonitoring",
+    "spring.datasource.username=postgres",
+    "spring.datasource.password=postgres",
+    "spring.rabbitmq.host=localhost",
+    "spring.rabbitmq.port=5672",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class LogIngestionServiceApplicationTests {
 
     @Test
