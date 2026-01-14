ad62857fe62f73dbed2bbc494f9302c1f422cf23
Kere Ekpenyong <Kere.Ekpenyong@ibm.com>
2026-01-13 20:49:19 -0500

    test for code analysis

--- backend/alert-service/src/test/java/com/ibm/aimonitoring/alert/AlertServiceApplicationTests.java
+++ backend/alert-service/src/test/java/com/ibm/aimonitoring/alert/AlertServiceApplicationTests.java
@@ -1,17 +1,14 @@
-package com.ibm.aimonitoring.processor;
+package com.ibm.aimonitoring.alert;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import com.ibm.aimonitoring.processor.consumer.LogConsumer;
 import org.junit.jupiter.api.Test;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * Integration test for Spring Boot application context.
+ * Integration test for Alert Service Application.
  * Uses mocked external dependencies to avoid requiring actual services.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,30 +18,21 @@ import org.springframework.web.client.RestTemplate;
     "spring.datasource.password=postgres",
     "spring.jpa.hibernate.ddl-auto=create-drop",
     "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect",
-    "spring.rabbitmq.host=localhost",
-    "spring.rabbitmq.port=5672"
+    "spring.mail.host=localhost",
+    "spring.mail.port=1025"
 })
-class LogProcessorServiceApplicationTests {
+class AlertServiceApplicationTests {
 
     @MockBean
-    private RabbitTemplate rabbitTemplate;
-    
-    @MockBean
-    private RabbitAdmin rabbitAdmin;
-    
-    @MockBean
-    private ElasticsearchClient elasticsearchClient;
+    private JavaMailSender javaMailSender;
     
     @MockBean
     private RestTemplate restTemplate;
-    
-    @MockBean
-    private LogConsumer logConsumer;
 
     @Test
     void contextLoads() {
         // This test verifies that the Spring application context loads successfully
-        // with mocked external dependencies (RabbitMQ, Elasticsearch, RestTemplate)
+        // with mocked external dependencies (JavaMailSender, RestTemplate)
     }
 }
 
